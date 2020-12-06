1ba364da8ca6cc054ec00589b8e285f9069a98c8
AugmentedR3altiy <adv.prg.aug@gmail.com>
2020-12-06 17:53:26 +0100

    shorten!!!

--- 6/task2.py
+++ 6/task2.py
@@ -1,6 +1,1 @@
-with open('data')as f:d=f.read()[:-1].split('\n\n')
-s=0
-for e in d:
-    r=[1 if all([i in j for j in e.split('\n')])else 0 for i in e.split('\n')[0]]
-    s+=sum(r)
-print(s)
+with open('data')as f:print(sum([sum([1 if all([i in j for j in e.split('\n')])else 0 for i in e.split('\n')[0]])for e in f.read()[:-1].split('\n\n')]))
